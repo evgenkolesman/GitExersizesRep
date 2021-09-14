21a34c7299d5de5ebd9af5edd67807c4204f1ee6
evgenkolesman <glavstroi_e@mail.ru>
2021-09-14 09:13:04 +0300

    2.9 change sqrt opp and delete commit

--- src/main/java/gitpack/First.java
+++ src/main/java/gitpack/First.java
@@ -4,4 +4,20 @@ public class First {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+
+    public double sqrt(double a) {
+        return Math.sqrt(a);
+    }
 }
