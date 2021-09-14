580d9bdaeb18f8bda96148342aa7dae63fbb7c98
evgenkolesman <glavstroi_e@mail.ru>
2021-09-14 09:56:40 +0300

    2.15 commit

--- src/main/java/gitpack/First.java
+++ src/main/java/gitpack/First.java
@@ -7,16 +7,6 @@ public class First {
         return a + b;
     }
 
-    public int add(int a, int b, int c) {
-        System.out.println("This is add method for 3 params");
-        return a + b + c;
-    }
-
-    public int add(int a, int b, int c, int d) {
-        System.out.println("This is add method for 4 params");
-        return a + b + c + d;
-    }
-
     public int sub(int a, int b) {
         return a - b;
     }
